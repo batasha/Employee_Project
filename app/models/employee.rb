a4e9ba43b0b8a27d859d63287bd743011528658a
= <=>
2013-05-16 13:13:22 -0400

    inverse_of between team and supervisor seems to be working

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ActiveRecord::Base
-  attr_accessible :name, :supervisor_id, :subordinate_ids
+  attr_accessible :name, :supervisor_id, :subordinate_ids, :supervised_team_ids
 
   has_many :subordinates, :class_name => "Employee",
            :foreign_key => :supervisor_id
@@ -7,7 +7,8 @@ class Employee < ActiveRecord::Base
   belongs_to :supervisor, :class_name => "Employee"
 
   has_many :supervised_teams, :class_name => "Team",
-           :inverse_of => :supervisor
+           :inverse_of => :supervisor,
+           :foreign_key => :supervisor_id
 
   has_many :team_memberships
   has_many :teams, :through => :team_memberships
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,8 @@ class Team < ActiveRecord::Base
   attr_accessible :name, :supervisor_id
 
   has_many :team_memberships
-  has_many :employees, :through => :team_memberships
+  has_many :members, :through => :team_memberships,
+           :source => :employee
   belongs_to  :supervisor, :class_name => "Employee",
               :inverse_of => :supervised_teams
 end
